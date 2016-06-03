ebb2a2c04baf5a4c22baa3b6cf63ddf7cc0bc692
JD Trout <jd.trout@stepsaway.com>
2016-06-02 20:57:47 -0700

    Added text tools and ability to set syslog-ng sources with ease

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 NAME = stepsaway/baseimage
 # Based on phussion/baseimage-docker
-VERSION = 1.0.0
+VERSION = 1.0.1
 
 .PHONY: build test tag_latest release
 
@@ -16,5 +16,6 @@ tag_latest:
 release: test tag_latest
 	@if ! docker images $(NAME) | awk '{ print $$2 }' | grep -q -F $(VERSION); then echo "$(NAME) version $(VERSION) is not yet built. Please run 'make build'"; false; fi
 	@if ! head -n 1 Changelog.md | grep -q 'release date'; then echo 'Please note the release date in Changelog.md.' && false; fi
-	docker push $(NAME)
+	docker push $(NAME):latest
+	docker push $(NAME):$(VERSION)
 	@echo "*** Don't forget to create a tag. git tag rel-$(VERSION) && git push origin rel-$(VERSION)"
--- image/services/syslog-ng/syslog-ng.sh
+++ image/services/syslog-ng/syslog-ng.sh
@@ -14,6 +14,7 @@ cp $SYSLOG_NG_BUILD_PATH/syslog_ng_default /etc/default/syslog-ng
 touch /var/log/syslog
 chmod u=rw,g=r,o= /var/log/syslog
 cp $SYSLOG_NG_BUILD_PATH/syslog-ng.conf /etc/syslog-ng/syslog-ng.conf
+cp $SYSLOG_NG_BUILD_PATH/syslog-ng-sources.conf /etc/syslog-ng/syslog-ng-sources.conf
 
 ## Install syslog to "docker logs" forwarder.
 mkdir /etc/service/syslog-forwarder
@@ -22,3 +23,9 @@ cp $SYSLOG_NG_BUILD_PATH/syslog-forwarder.runit /etc/service/syslog-forwarder/ru
 ## Install logrotate.
 $minimal_apt_get_install logrotate
 cp $SYSLOG_NG_BUILD_PATH/logrotate_syslogng /etc/logrotate.d/syslog-ng
+
+## Install papertrail certificates
+mkdir /etc/syslog-ng/cert.d
+pushd /etc/syslog-ng/cert.d
+curl https://papertrailapp.com/tools/papertrail-bundle.tar.gz | sudo tar xzf -
+popd
